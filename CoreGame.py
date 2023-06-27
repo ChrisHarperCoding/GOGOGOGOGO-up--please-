984f79a05392ea34cacd73d2854a9de8c2d80fbb
ChrisHarperCoding <129756841+ChrisHarperCoding@users.noreply.github.com>
2023-06-27 02:51:15 +0100

    Update CoreGame.py

--- CoreGame.py
+++ CoreGame.py
@@ -16,16 +16,10 @@ FONT = pygame.font.Font(None, 36)
 # Set up the display
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# Create a list of stars for the starfield effect
-stars = []
-for _ in range(100):
-    x = random.randint(0, WIDTH)
-    y = random.randint(0, HEIGHT)
-    stars.append([x, y])
-
-# Set up the player and obstacles
+# Set up the player and obstacle
 player = pygame.Rect(WIDTH / 2, HEIGHT - PLAYER_SIZE - 10, PLAYER_SIZE, PLAYER_SIZE)
-obstacles = []
+obstacles = pygame.sprite.Group()
+powerups = pygame.sprite.Group()  # Create a group to store power-ups
 
 # Set up the clock
 clock = pygame.time.Clock()
@@ -33,6 +27,52 @@ clock = pygame.time.Clock()
 # Game state
 game_state = "start"
 score = 0
+
+# Power-up class
+class PowerUp(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((30, 30))
+        self.image.fill((0, 255, 0))  # Green color for invincibility power-up
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+    
+    def update(self):
+        # Add any additional behavior for the power-up
+        pass
+
+# Starfield class
+class Starfield(pygame.sprite.Sprite):
+    def __init__(self, width, height, num_stars):
+        super().__init__()
+        self.image = pygame.Surface((width, height))
+        self.rect = self.image.get_rect()
+        self.stars = []
+        self.generate_stars(width, height, num_stars)
+    
+    def generate_stars(self, width, height, num_stars):
+        for _ in range(num_stars):
+            x = random.randint(0, width)
+            y = random.randint(0, height)
+            size = random.randint(1, 3)
+            self.stars.append((x, y, size))
+    
+    def update(self):
+        self.image.fill((0, 0, 0))
+        for star in self.stars:
+            x, y, size = star
+            pygame.draw.circle(self.image, WHITE, (x, y), size)
+    
+    def scroll(self):
+        for i in range(len(self.stars)):
+            x, y, size = self.stars[i]
+            y += SPEED // 2  # Adjust the scroll speed
+            if y > HEIGHT:
+                y = 0
+            self.stars[i] = (x, y, size)
+
+# Create the starfield
+starfield = Starfield(WIDTH, HEIGHT, 100)  # Adjust the number of stars as desired
 
 # Game loop
 running = True
@@ -43,11 +83,13 @@ while running:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                if game_state == "start" or game_state == "game over":
+                if game_state == "start":
                     game_state = "running"
+                elif game_state == "game over":
+                    game_state = "start"
                     score = 0
-                    player = pygame.Rect(WIDTH / 2, HEIGHT - PLAYER_SIZE - 10, PLAYER_SIZE, PLAYER_SIZE)
-                    obstacles = []
+                    player.x = WIDTH / 2
+                    obstacles.empty()
 
     # Game logic
     if game_state == "running":
@@ -59,38 +101,50 @@ while running:
             player.x += SPEED
 
         # Add new obstacles
-        if len(obstacles) == 0 or obstacles[-1][1] > 20:
-            obstacle_x = random.randrange(WIDTH - OBSTACLE_WIDTH)
-            obstacles.append([obstacle_x, -OBSTACLE_HEIGHT])
+        if not obstacles or obstacles.sprites()[-1].rect.y > 20:
+            obstacle = pygame.sprite.Sprite()
+            obstacle.rect = pygame.Rect(random.randrange(WIDTH - OBSTACLE_WIDTH), -OBSTACLE_HEIGHT, OBSTACLE_WIDTH, OBSTACLE_HEIGHT)
+            obstacle.image = pygame.Surface((OBSTACLE_WIDTH, OBSTACLE_HEIGHT))
+            obstacle.image.fill(RED)
+            obstacles.add(obstacle)
+
+        # Add new power-ups
+        if not powerups or powerups.sprites()[-1].rect.y > 200:
+            powerup = PowerUp(random.randrange(WIDTH - 30), -30)
+            powerups.add(powerup)
 
         # Obstacle movement and collision detection
         for obstacle in obstacles:
-            obstacle[1] += SPEED
-            if player.colliderect(pygame.Rect(obstacle[0], obstacle[1], OBSTACLE_WIDTH, OBSTACLE_HEIGHT)):
+            obstacle.rect.y += SPEED
+            if player.colliderect(obstacle.rect):
                 game_state = "game over"
-            if obstacle[1] > HEIGHT:
+            if obstacle.rect.y > HEIGHT:
                 obstacles.remove(obstacle)
                 score += 1
 
+        # Power-up movement and collision detection
+        for powerup in powerups:
+            powerup.rect.y += SPEED
+            if player.colliderect(powerup.rect):
+                powerups.remove(powerup)
+                # Add any power-up behavior here
+                score += 5  # Increase score for collecting power-ups
+
     # Update the starfield
-    for star in stars:
-        star[1] += SPEED
-        if star[1] > HEIGHT:
-            star[1] = random.randint(-HEIGHT, 0)
+    starfield.scroll()
 
     # Drawing
-    # Draw the starfield background
     screen.fill((0, 0, 0))
-    for star in stars:
-        pygame.draw.circle(screen, WHITE, (star[0], star[1]), 1)
+    starfield.update()
+    screen.blit(starfield.image, starfield.rect)
 
     if game_state == "start":
         text = FONT.render("Press SPACE to start", True, WHITE)
         screen.blit(text, (WIDTH / 2 - text.get_width() / 2, HEIGHT / 2 - text.get_height() / 2))
     elif game_state == "running":
         pygame.draw.rect(screen, WHITE, player)
-        for obstacle in obstacles:
-            pygame.draw.rect(screen, RED, (obstacle[0], obstacle[1], OBSTACLE_WIDTH, OBSTACLE_HEIGHT))
+        obstacles.draw(screen)
+        powerups.draw(screen)
         text = FONT.render(f"Score: {score}", True, WHITE)
         screen.blit(text, (10, 10))
     elif game_state == "game over":
@@ -105,4 +159,3 @@ while running:
 
 # Quit Pygame
 pygame.quit()
-
